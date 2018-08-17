fd2df7ae5d643e94f2530f7665b29aaf10047dee
elaubinger <elaubinger@yahoo.com>
2018-08-17 14:27:17 -0400

    Initial Commit (semi functional in debug, not working for general usage)

---
+++ bat_default_exe/Program.cs
@@ -0,0 +1,118 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Text.RegularExpressions;
+
+namespace bat_default_exe
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            if (args.Length < 1)
+                return;
+
+            var file = new FileInfo(args[0]);
+
+            if (!file.Exists)
+                return;
+
+            var stream = file.OpenText();
+
+            var commands = new List<string>();
+            while (!stream.EndOfStream)
+                commands.Add(stream.ReadLine());
+            try
+            {
+                ExecuteCommands(commands);
+            }
+            catch(Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+                Console.Write("Enter any key to exit: ");
+                Console.ReadKey();
+            }
+
+            #if DEBUG
+            Console.ReadKey();
+            #endif
+        }
+
+        // https://stackoverflow.com/questions/5519328/executing-batch-file-in-c-sharp
+        // https://stackoverflow.com/questions/1704791/is-my-process-waiting-for-input
+        static void ExecuteCommands(List<string> commands)
+        {
+            ProcessStartInfo processInfo;
+            Process process;
+
+            processInfo = new ProcessStartInfo("cmd.exe");
+            processInfo.CreateNoWindow = true;
+            processInfo.UseShellExecute = false;
+
+            processInfo.RedirectStandardOutput = true;
+            processInfo.RedirectStandardInput = true;
+            processInfo.RedirectStandardError = true;
+
+            process = Process.Start(processInfo);
+
+            var input = process.StandardInput;
+            var thread = process.Threads[0];
+
+            process.OutputDataReceived += (object sender, DataReceivedEventArgs e) =>
+                Console.WriteLine(e.Data);
+
+            process.ErrorDataReceived += (object sender, DataReceivedEventArgs e) =>
+            {
+                if (!String.IsNullOrWhiteSpace(e.Data))
+                    throw new Exception($"ERROR> {e.Data}");
+            };
+
+            process.BeginOutputReadLine();
+            process.BeginErrorReadLine();
+
+            Console.WriteLine("BEGIN OUTPUT");
+
+            foreach (var command in commands)
+            {
+                input.WriteLine(command);
+
+                /*
+                 * WIP functionality to take user input
+                var parsed = new BATCommand(command);
+
+                if (parsed.Command == "SET" && parsed.Flags.Contains(@"/P"))
+                    input.WriteLine(Console.ReadLine());
+                */
+
+            }
+
+            Console.WriteLine("END OUTPUT");
+
+            process.Close();
+        }
+
+        private class BATCommand
+        {
+            public readonly string Command;
+            public readonly HashSet<string> Flags = new HashSet<string>();
+            public readonly List<string> Parameters = new List<string>();
+
+            public BATCommand(string command)
+            {
+                command = command.Trim();
+                var elems = command.Split(null);
+                for(int i = 0; i < elems.Length; i++)
+                {
+                    var token = elems[i];
+
+                    if (i == 0)
+                        Command = token;
+                    else if (Regex.IsMatch(token, @"^\s*/"))
+                        Flags.Add(token);
+                    else Parameters.Add(token);
+                }
+            }
+        }
+    }
+}
---
+++ bat_default_exe/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("bat_default_exe")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("bat_default_exe")]
+[assembly: AssemblyCopyright("Copyright ©  2018")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("5aa1422a-52c1-4a15-92c3-9867e92fc9c2")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
